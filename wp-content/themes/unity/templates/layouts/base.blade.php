721c82eb43b98bea26ac00550c0d0f9b4cc821d4
Alisa Herr <alisa@unitymakes.us>
2017-01-16 10:10:04 -0500

    Remove extraneous ; from blade templates

--- wp-content/themes/unity/templates/layouts/base.blade.php
+++ wp-content/themes/unity/templates/layouts/base.blade.php
@@ -6,7 +6,7 @@
       <!-- Google Tag Manager (noscript) -->
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WWVMGLG" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
       <!-- End Google Tag Manager (noscript) -->
-    @endif;
+    @endif
     <!--[if IE]>
       <div class="alert alert-warning">
         {!! __('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage') !!}
