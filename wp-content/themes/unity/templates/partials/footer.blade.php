66b8f771be728b3f859d26a778903c10f6226963
Alisa Herr <alisa@unitymakes.us>
2017-01-23 16:00:35 -0500

    Fix links to email and social media

--- wp-content/themes/unity/templates/partials/footer.blade.php
+++ wp-content/themes/unity/templates/partials/footer.blade.php
@@ -10,14 +10,14 @@
         <h5>Get In Touch</h5>
         <p>
           <tel>919-391-4961</tel><br />
-          <a href="{{ eae_encode_str('hello@unitymakes.us') }}">
+          <a href="mailto:{{ eae_encode_str('hello@unitymakes.us') }}">
             {{ eae_encode_str('hello@unitymakes.us') }}
           </a>
         </p>
         <p>
-          <a href="#"><i class="fa fa-lg fa-twitter"></i></a>&nbsp;&nbsp;
-          <a href="#"><i class="fa fa-lg fa-facebook"></i></a>&nbsp;&nbsp;
-          <a href="#"><i class="fa fa-lg fa-linkedin"></i></a>&nbsp;&nbsp;
+          <a href="https://twitter.com/unitymakesus"><i class="fa fa-lg fa-twitter"></i></a>&nbsp;&nbsp;
+          <a href="https://www.facebook.com/unitymakesus/"><i class="fa fa-lg fa-facebook"></i></a>&nbsp;&nbsp;
+          <a href="https://www.linkedin.com/company/unity-digital-agency"><i class="fa fa-lg fa-linkedin"></i></a>&nbsp;&nbsp;
         </p>
       </div>
       <div class="col m3">
@@ -33,7 +33,7 @@
   </div>
   <div class="footer-copyright">
     <div class="container">
-    <i class="cc cc-by"></i> {{ the_time('Y') }} Unity Digital Agency. <a href="/privacy-policy">Privacy Policy.</a> Photo credits <a href="http://www.wocintechchat.com/" target="_blank">#WOCinTech</a> and <a href="https://unsplash.com/" target="_blank">Unsplash</a>.
+    <i class="cc cc-by"></i> {{ the_time('Y') }} Unity Digital Agency. <a href="/privacy-policy/">Privacy Policy.</a> Photo credits <a href="http://www.wocintechchat.com/" target="_blank">#WOCinTech</a> and <a href="https://unsplash.com/" target="_blank">Unsplash</a>.
     </div>
   </div>
 </footer>
